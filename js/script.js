abacbf482e137e681994aecff2a0025be74152ba
nazi-maisuradze <146000732+nazi-maisuradze@users.noreply.github.com>
2023-11-16 01:01:12 +0400

    burger

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let navigationelement = document.getElementById ('navigationblock');
 let burgerbar = document.getElementById ('burgerblock');
 
 burgerbar.addEventListener ('click' , function() {
-    navigationelement.classList.add('nav');
+    navigationelement.classList.toggle('nav');
 })
